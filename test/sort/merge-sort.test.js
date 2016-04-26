5ac6259ffa04e636bc9aae136a473171358b433a
nonoroazoro <xiaochao.k@gmail.com>
2016-04-26 20:54:05 +0800

    add test

---
+++ test/sort/merge-sort.test.js
@@ -0,0 +1,22 @@
+﻿const mocha = require("mocha");
+const should = require("should");
+
+const sort = require("../../lib").sort;
+
+describe("sort/merge-sort", function ()
+{
+    const origin = [2, 6, 5, 3, 1, 8, 7, 4];
+    const targetASC = [1, 2, 3, 4, 5, 6, 7, 8];
+    it(`asc - should sort array: [${origin}] to: [${targetASC}].`, function (done)
+    {
+        sort.merge(origin).should.deepEqual(targetASC);
+        done();
+    });
+
+    const targetDESC = [8, 7, 6, 5, 4, 3, 2, 1];
+    it(`desc - should sort array: [${origin}] to: [${targetDESC}].`, function (done)
+    {
+        sort.merge(origin, true).should.deepEqual(targetDESC);
+        done();
+    });
+});
