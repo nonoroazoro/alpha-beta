67b4ce4a1cf5124772b42b244b09be1805f830b8
nonoroazoro <xiaochao.k@gmail.com>
2017-11-28 16:50:01 +0800

    add shuffle

--- test/sort/merge-sort.test.js
+++ test/sort/merge-sort.test.js
@@ -1,20 +1,17 @@
-﻿const mocha = require("mocha");
-const should = require("should");
+﻿const sort = require("../../lib").sort;
 
-const sort = require("../../lib").sort;
-
-describe("sort/merge-sort", function ()
+describe("sort/merge-sort", () =>
 {
     const origin = [2, 6, 5, 3, 1, 8, 7, 4];
     const targetASC = [1, 2, 3, 4, 5, 6, 7, 8];
-    it(`asc - should sort array: [${origin}] to: [${targetASC}].`, function (done)
+    it(`asc - should sort array: [${origin}] to: [${targetASC}].`, (done) =>
     {
         sort.merge(origin).should.deepEqual(targetASC);
         done();
     });
 
     const targetDESC = [8, 7, 6, 5, 4, 3, 2, 1];
-    it(`desc - should sort array: [${origin}] to: [${targetDESC}].`, function (done)
+    it(`desc - should sort array: [${origin}] to: [${targetDESC}].`, (done) =>
     {
         sort.merge(origin, true).should.deepEqual(targetDESC);
         done();
---
+++ test/sort/shuffle.test.js
@@ -0,0 +1,14 @@
+﻿const should = require("should");
+
+const sort = require("../../lib").sort;
+
+describe("sort/shuffle", () =>
+{
+    const origin = [1, 2, 3, 4, 5, 6, 7, 8];
+    const validation = [1, 2, 3, 4, 5, 6, 7, 8];
+    it(`should shuffle array: [${origin}].`, (done) =>
+    {
+        should.notDeepEqual(sort.shuffle(origin), validation);
+        done();
+    });
+});
